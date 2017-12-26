72535a4ca7a9d8c7a4c3e0134938452531b032bc
hiepph <hoanghiepjp96@gmail.com>
2017-12-27 03:00:25 +0700

    feat: Skin mask for hand ROI
    Thanks reference from https://github.com/asingh33/CNNGestureRecognizer

--- hand_detection.py
+++ hand_detection.py
@@ -10,15 +10,38 @@ from model import CNN, predict
 MODEL_PATH = 'model/cnn.pth'
 
 
+def skin_mask(roi):
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+    # Apply skin color range
+    low_range = np.array([0, 50, 80])
+    upper_range = np.array([30, 200, 255])
+    mask = cv2.inRange(hsv, low_range, upper_range)
+
+    skinkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    mask = cv2.erode(mask, skinkernel, iterations=1)
+    mask = cv2.dilate(mask, skinkernel, iterations=1)
+
+    # blur
+    mask = cv2.GaussianBlur(mask, (15, 15), 1)
+
+    # bitwise and mask original frame
+    res = cv2.bitwise_and(roi, roi, mask=mask)
+    # color to grayscale
+    res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
+    return res
+
+
 def main():
     # Load pre-trained model
-    if not os.path.exists(MODEL_PATH):
-        print("'{}' not found".format(MODEL_PATH))
-        return
+    # if not os.path.exists(MODEL_PATH):
+    #     print("'{}' not found".format(MODEL_PATH))
+    #     return
 
-    model = CNN()
-    print(model)
-    model.load_state_dict(torch.load(MODEL_PATH))
+    # model = CNN()
+    # print(model)
+    # model.load_state_dict(torch.load(MODEL_PATH))
 
     # Camera
     cap = cv2.VideoCapture(0)
@@ -33,10 +56,13 @@ def main():
 
         # Take ROI crop from frame
         roi = frame[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+        # Preprocess roi
+        roi = skin_mask(roi)
+        # shơw it
         cv2.imshow('ROI', roi)
 
         # Predict and show result
-        print(predict(model, roi))
+        # print(predict(model, roi))
 
         # Draw ROI
         cv2.rectangle(frame, top_left, bottom_right, (0,255,0), 1)
